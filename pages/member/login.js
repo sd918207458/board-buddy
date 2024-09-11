542f664965605225ad148ef9b6b3a72cb1f98de3
sd918207458 <sd918207458@gmail.com>
2024-09-11 10:20:38 +0800

    共用樣式設定

---
+++ pages/member/login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "@/components/header";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+
+export default function Login() {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+}
