5fa2ed90d2f0e9e48a40ae4c5e332acf8c40dd8e
Max <cdu3980712@gmail.com>
2024-09-11 11:26:42 +0800

    引入daisy UI 基本 Accordion

--- pages/product-list-page.jsx
+++ pages/product-list-page.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import ProductList from '../components/ProductList'; // 导入 ProductList 组件
+import ProductList from '../components/ProductList/ProductList'; // 导入 ProductList 组件
+import Sidebar from '@/components/ProductList/Sidebar/Sidebar';
 
 const ProductListPage = () => {
   return (
     <div>
       <ProductList />
+      <Sidebar></Sidebar>
     </div>
   );
 };
