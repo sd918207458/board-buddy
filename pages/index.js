bda36f05cf0329e933b3e288a17636184e0415d5
Max <cdu3980712@gmail.com>
2024-09-18 10:44:54 +0800

    刪除 geistMono 等等

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,3 @@ export default function Home() {
   return <div>Home</div>;
 }
 
-
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
