dad03f6a625533e2099ffa5a590bac4efa4c488e
Max32722 <168209938+Max32722@users.noreply.github.com>
2024-09-22 11:16:44 +0800

    刪掉pagination

---
+++ pages/product/product-list.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Sidebar from "@/components/sidebar/sidebar.js";
+import ProductCard from "@/components/productcard/productcard";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+import Pagination from "@/components/pagination/Pagination";
+import Breadcrumbs from "@/components/Breadcrumbs";
+const ProductList = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="flex">
+        {/* Sidebar on the left */}
+        <aside className="w-70">
+          <Breadcrumbs />
+          <Sidebar />
+        </aside>
+
+        {/* Main content on the right */}
+        <main className="flex-1 p-4">
+          {/* Product Card and other components */}
+          <div>
+            <ProductCard />
+            <Pagination />
+            {/* 其他卡片可以繼續加入 */}
+          </div>
+        </main>
+      </div>
+
+      <Footer />
+    </>
+  );
+};
+export default ProductList;
