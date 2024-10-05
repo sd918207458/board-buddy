9f0cc37db94f89d91d256ae06eb2fcb82facce46
Max <cdu3980712@gmail.com>
2024-10-05 15:37:36 +0800

    添加hover樣式

--- components/card/Card.jsx
+++ components/card/Card.jsx
@@ -68,33 +68,50 @@ const Card = () => {
           {/* 商品网格布局部分 */}
 
           <div className="w-full md:w-3/4 mx-auto p-6">
-            {/* here */}
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
               {Array.isArray(products) &&
                 products.map((product) => (
                   <a
                     key={product.product_id}
                     href="#"
-                    className="flex flex-col"
+                    className="relative flex flex-col group"
                   >
-                    <Image
-                      className="hover:grow hover:shadow-lg"
-                      src={product.image}
-                      width={400}
-                      height={400}
-                      alt={product.product_name}
-                    />
-                    <div className="pt-3 flex items-center justify-between">
+                    {/* 商品圖片容器，確保圖片和圓角一致 */}
+                    <div className="relative rounded-lg overflow-hidden">
+                      <Image
+                        className="transition-all duration-300 group-hover:blur-md rounded-lg" // 確保圖片模糊時保持圓角
+                        src={product.image}
+                        width={400}
+                        height={400}
+                        alt={product.product_name}
+                      />
+                    </div>
+
+                    {/* Hover 出現的 Overlay 效果，保持一致的圓角 */}
+                    <div className="absolute inset-0 bg-[#003E52] bg-opacity-80 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-lg">
+                      <h1 className="text-xl font-bold mb-4 text-white">
+                        {product.product_name}
+                      </h1>
+                      <a
+                        href="#"
+                        className="py-2 px-6 text-white border border-white rounded-lg hover:bg-white hover:text-[#003E52] transition-all"
+                      >
+                        加入購物車
+                      </a>
+                    </div>
+
+                    {/* 商品名稱和價格部分 */}
+                    <div className="pt-3 flex items-center justify-between transition-all duration-300 group-hover:blur-md">
                       <p>{product.product_name}</p>
                       <svg
                         className="h-6 w-6 fill-current text-gray-500 hover:text-black"
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 24 24"
                       >
-                        <path d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z" />
+                        <path d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5C14.719,4.698,17.281,4.702,18.791,6.205z" />
                       </svg>
                     </div>
-                    <p className="pt-1 text-white-900  font-bold">
+                    <p className="pt-1 text-white-900 font-bold transition-all duration-300 group-hover:blur-md">
                       ${product.price}
                     </p>
                   </a>
