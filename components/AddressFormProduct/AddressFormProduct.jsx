9ef431177292d537965de50f0466c4648c98a38f
Max <cdu3980712@gmail.com>
2024-10-11 16:27:45 +0800

    更新結帳頁面樣式 變更簡潔

--- components/AddressFormProduct/AddressFormProduct.jsx
+++ components/AddressFormProduct/AddressFormProduct.jsx
@@ -234,7 +234,7 @@ const AddressFormProduct = () => {
 
   return (
     <div>
-      <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+      <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800 mt-8">
         <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">
           運送方式
         </h2>
